4428f7818299ad58ec9b256bd2114709edb61484
fahali-blk <faheem.ali@blackrock.com>
2020-05-10 18:51:54 +0530

    Adding the complete transcript to db

--- app/controllers/video_insert_controller.rb
+++ app/controllers/video_insert_controller.rb
@@ -1,11 +1,21 @@
 class VideoInsertController < ApplicationController
   def insert
-    @object = Video.new(
-        title: 'Video 2',
-        transcript: 'This is transcript2',
-        s3url:'s3 url2',
-        folder_path: '/folder2/path'
-    )
-    @object.save
+    @count = 1
+    while @count < 4
+      fileCount  = @count.to_s
+      fileLocation = "app/assets/transcripts/transcript_" + fileCount + ".json"
+      @f = File.open(fileLocation, "rb")
+      @transcript = @f.read
+      title = "ted_video_" + fileCount + "/mp4"
+      @object = Video.new(
+          title: title,
+          transcript: @transcript,
+          s3url:'s3 url2',
+          folder_path: '/folder2/path'
+      )
+      @object.save
+      @f.close
+      @count += 1
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Video.create(
-    title: 'Ted Talk 1: ',
-    transcript: 'transcript_1.json',
-    s3url: 'https://console.aws.amazon.com/s3/buckets/hack2020-dont-delete/video1/?region=ap-south-1&tab=overview',
-    folder_path: 'app/assets/transcripts',
-    created_at: '2020-05-08 09:11:03',
-    updated_at: '2020-05-08 09:11:03'
-)
-
-Video.create(
-    title: 'Ted Talk 2: ',
-    transcript: 'transcript_2.json',
-    s3url: 'https://console.aws.amazon.com/s3/buckets/hack2020-dont-delete/video2/?region=ap-south-1&tab=overview',
-    folder_path: 'app/assets/transcripts',
-    created_at: '2020-05-08 09:12:03',
-    updated_at: '2020-05-08 09:12:03'
-)
-
-Video.create(
-    title: 'Ted Talk 3: ',
-    transcript: 'transcript_3.json',
-    s3url: 'https://console.aws.amazon.com/s3/buckets/hack2020-dont-delete/video3/?region=ap-south-1&tab=overview',
-    folder_path: 'app/assets/transcripts',
-    created_at: '2020-05-08 09:13:03',
-    updated_at: '2020-05-08 09:13:03'
-)
+v = VideoInsertController.new
+v.insert
