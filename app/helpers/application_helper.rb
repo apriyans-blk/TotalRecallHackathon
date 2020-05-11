7e48a77df63d3dd21682e492f5aaeddacfea6be4
apriyans-blk <akshat.priyansh@blackrock.com>
2020-05-11 15:53:58 +0530

    + v5-adding-thumbnails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def image_path(video_title)
+    id = video_title.split('.')[0].delete("^0-9")
+    url = image_url 'thumbnail_'+id.to_s+'.png'
+    image_url url
+  end
 end
